8d5090f5342ace095a9200f84f531912fb9320e7
yanivkalfa <netameta1@yahoo.com>
2015-09-15 21:31:44 +0000

    init commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name websiteassemblylineApp
+ * @description
+ * # websiteassemblylineApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('websiteassemblylineApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name websiteassemblylineApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the websiteassemblylineApp
+ */
+angular.module('websiteassemblylineApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
