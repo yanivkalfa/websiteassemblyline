cff99916e415ec7854169b3b55c2d35db57c72e3
yaniv kalfa <netameta1@yahoo.com>
2015-09-15 21:49:36 -0400

    html 5 enabled

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,19 +16,34 @@ angular
     'ngRoute',
     'ngSanitize'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  .config([
+    '$injector', '$locationProvider',
+    function($injector, $locationProvider) {
+
+      // setting $auth provider configuration
+      //ng.extend($authProvider, ENV.authProvider);
+
+      // html 5 mode getting rid of #
+      $locationProvider.html5Mode(true);
+
+      /* adding an interceptor
+      $httpProvider.interceptors.push(['$injector', function ($injector) {
+        return $injector.get('AuthInterceptorService');
+      }]);*/
+
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/main.html',
+          controller: 'MainCtrl',
+          controllerAs: 'main'
+        })
+        .when('/about', {
+          templateUrl: 'views/about.html',
+          controller: 'AboutCtrl',
+          controllerAs: 'about'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    }]
+);
