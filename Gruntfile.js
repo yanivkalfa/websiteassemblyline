cae1cb9cdbbc35c513506a0de2385499e8116e1d
yaniv kalfa <netameta1@yahoo.com>
2015-09-16 23:44:37 -0400

    change destination

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,8 +22,8 @@ module.exports = function (grunt) {
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: '/var/www/websiteassemblyline/dist'
-    //dist: 'dist'
+    //dist: '/var/www/websiteassemblyline/dist'
+    dist: 'dist'
   };
 
   // Define the configuration for all the tasks
