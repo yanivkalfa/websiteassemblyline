50bd02e89c590ca4afc7712d34928d9345d03e69
yanivkalfa <netameta1@yahoo.com>
2015-11-11 20:12:03 -0500

    Update about.js

--- test/spec/controllers/about.js
+++ test/spec/controllers/about.js
@@ -2,13 +2,14 @@
 
 describe('Controller: AboutCtrl', function () {
 
+
   // load the controller's module
   beforeEach(module('websiteassemblylineApp'));
 
   var AboutCtrl,
     scope;
 
-  // Initialize the controller and a mock scope
+  // Initialize the controller and a mock scope 
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
     AboutCtrl = $controller('AboutCtrl', {
