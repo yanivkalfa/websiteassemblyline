cf88deef1130111cf146f5775cda7ece883f0e08
yanivkalfa <netameta1@yahoo.com>
2015-11-14 03:39:43 -0500

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function (grunt) {
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
-  // Automatically load required Grunt tasks
+  // Automatically load required Grunt tasks 
   require('jit-grunt')(grunt, {
     useminPrepare: 'grunt-usemin',
     ngtemplates: 'grunt-angular-templates',
