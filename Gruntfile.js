3139485397b78697b07b62873c30373f95f36caa
yanivkalfa <netameta1@yahoo.com>
2015-11-28 19:39:16 -0500

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@
 
 // # Globbing
 // for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js' 
+// 'test/spec/{,*/}*.js'
 // use this if you want to recursively match all subfolders:
 // 'test/spec/**/*.js' 
 
